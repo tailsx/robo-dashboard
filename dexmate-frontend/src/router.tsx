ed79bf35904e6b6ab3eb54bb6f119a5a8b025ed3
DC <chendavid383@gmail.com>
2025-11-06 12:39:13 -0800

    fixing jarring login

--- dexmate-frontend/src/router.tsx
+++ dexmate-frontend/src/router.tsx
@@ -4,11 +4,10 @@ import { NotFoundPage } from "@/pages/not-found-page";
 import { createBrowserRouter } from "react-router";
 import type { RouteObject } from "react-router";
 import { HomePage } from "@/pages/home-page";
-import { AppLayout } from "./layouts/app-layout";
+import { ProtectedAppLayout } from "./layouts/protected-app-layout";
 import { RobotsPage } from "./pages/robots-page";
 import { GroupsPage } from "./pages/groups-page";
 import { RobotsDetailPage } from "./pages/robots-detail-page";
-import { AuthenticatedRoute } from "./features/login/authenticate";
 import { GroupsDetailPage } from "./pages/group-detail-page";
 import { RobotCreatePage } from "./pages/robot-create-page";
 import { GroupCreatePage } from "./pages/group-create-page";
@@ -27,43 +26,37 @@ export const routes: RouteObject[] = [
   },
   {
     path: "/",
-    element: <AppLayout />,
+    element: <ProtectedAppLayout />,
     errorElement: <NotFoundPage />,
     children: [
       {
-        element: <AuthenticatedRoute />,
-        children: [
-          {
-            index: true,
-            element: <HomePage />,
-          },
-          {
-            path: "robots",
-            element: <RobotsPage />,
-          },
-          {
-            path: "robots/create",
-            element: <RobotCreatePage />,
-          },
-
-          {
-            path: "robots/:robotId",
-            element: <RobotsDetailPage />,
-            errorElement: <NotFoundPage />,
-          },
-          {
-            path: "groups",
-            element: <GroupsPage />,
-          },
-          {
-            path: "groups/create",
-            element: <GroupCreatePage />,
-          },
-          {
-            path: "groups/:groupId",
-            element: <GroupsDetailPage />,
-          },
-        ],
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "robots",
+        element: <RobotsPage />,
+      },
+      {
+        path: "robots/create",
+        element: <RobotCreatePage />,
+      },
+      {
+        path: "robots/:robotId",
+        element: <RobotsDetailPage />,
+        errorElement: <NotFoundPage />,
+      },
+      {
+        path: "groups",
+        element: <GroupsPage />,
+      },
+      {
+        path: "groups/create",
+        element: <GroupCreatePage />,
+      },
+      {
+        path: "groups/:groupId",
+        element: <GroupsDetailPage />,
       },
     ],
   },
